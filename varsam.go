3a5ed048062900a43982034b7caab402b31f1d28
Shane Husson <shane.a.husson@gmail.com>
2017-12-04 15:12:14 +1100

    init

---
+++ varsam.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"encoding/csv"
+	"io"
+	"os"
+	"strconv"
+	"fmt"
+	"strings"
+)
+
+
+func main() {
+	processGenotypes()
+}
+
+func processGenotypes() {
+	f, err := os.Open("data/gdb_10.tsv")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+
+
+	output := ""
+	reader := csv.NewReader(f)
+	reader.Comma = '\t'
+	rowIndex := 0
+	for {
+		row, err := reader.Read()
+		if rowIndex == 0 {
+			rowIndex++
+			continue
+		}
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		sampleId, err := strconv.Atoi(row[0])
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		variantKey := strings.Replace(row[1], ":", "", -1)
+
+		//AF, err := strconv.ParseFloat(row[2], 64)
+
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		output += fmt.Sprintf("_:sample%d <name> \"%d\" .\n", sampleId,  sampleId)
+		output += fmt.Sprintf("_:variant%s <name> \"%s\" .\n", variantKey,  variantKey)
+		output += fmt.Sprintf("_:sample%d <variant> _:variant%s .\n", sampleId,  variantKey)
+
+		rowIndex++
+	}
+	of, err := os.Create("data/test.rdf")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer of.Close()
+	of.WriteString(output)
+
+	fmt.Println(output)
+}
+
